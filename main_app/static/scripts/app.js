0c8c864f9016ebe6ff2fd843d9140a91fc88c082
bbakercello <bbakercello@gmail.com>
2022-10-05 15:23:31 -0400

    refactored detail page

--- main_app/static/scripts/app.js
+++ main_app/static/scripts/app.js
@@ -1,7 +1,10 @@
-// $(".navbar-burger").click(function () {
-//     // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-//     $(".navbar-burger").toggleClass("is-active");
-//     $(".navbar-menu").toggleClass("is-active");
-//   });
-
-//   console.log("Hi everyone")
+$(".navbar-burger").click(function () {
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+    
+  });
+  $(".dropdown").click(function (event) {
+    $(this).toggleClass("is-active");
+  });
+  console.log("Hi everyone")
